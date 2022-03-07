7b1c5bc7ddc9616008a2fab715bff5f5ea4187bc
Alex <userblade4@gmail.com>
2022-03-07 14:14:51 +0100

    Initial commit

---
+++ src/main/java/com/dws/practicaweb/PracticaWebApplication.java
@@ -0,0 +1,13 @@
+package com.dws.practicaweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticaWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PracticaWebApplication.class, args);
+    }
+
+}
